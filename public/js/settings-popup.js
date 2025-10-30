6c8939a5160e08867d1892c1c272728d2b228520
Sa Sa <ssalihsrz@gmail.com>
2025-10-30 12:51:55 -0400

    v3.1.169 - Lifeguard fix for autoClearOnZoneChange check

--- public/js/settings-popup.js
+++ public/js/settings-popup.js
@@ -266,7 +266,7 @@ async function checkForUpdates() {
         const data = await response.json();
         
         const latestVersion = data.tag_name.replace('v', '');
-        const currentVersion = '3.1.168';
+        const currentVersion = '3.1.169';
         
         if (button) {
             button.innerHTML = '<i class="fa-solid fa-check"></i> Check Complete';
